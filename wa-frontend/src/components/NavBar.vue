946a8c1b050e23f75e1bd377c627d5fab663b5c7
EricOstoni <eostoni@student.unipu.hr>
2023-06-24 04:26:13 +0200

    add homwview , add component navbar for homeview and style

---
+++ wa-frontend/src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav>
+    <a class="logo">Oil Shop Ostoni</a>
+    <a class="first">About us</a>
+    <a class="second">Our products</a>
+    <a>Contact</a>
+
+    <button class="btn-navbar">WEBSHOP</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style scoped>
+@import "../assets/style.css";
+</style>
--- wa-frontend/src/views/HomeView.vue
+++ wa-frontend/src/views/HomeView.vue
@@ -1,11 +1,66 @@
 <template>
-  <h1>this is the home page</h1>
+  <div>
+    <NavBar />
+  </div>
+
+  <div class="main-content">
+    <h1>OSTONI OIL</h1>
+    <h2>more than extra virgin oil</h2>
+  </div>
+
+  <div class="about-us">
+    <div class="container">
+      <h1>About us</h1>
+      <p>
+        OPG Ostoni is a family farm specialized in growing olives and producing
+        high quality extra virgin olive oil. Currently we cultivate 10 hectars
+        of olive groves in the area of Vodnjan with more than 1300 olive trees
+        at an altitude of 80 – 180 m.a.s.l. Visit us for a free olive oil
+        tasting in our olive mill located in olive grooves just outside the old
+        medieval town of Vodnjan or book one of our guided olive oil tasting
+        tours and discover why our extra virgin olive oil is frequently
+        proclaimed among the best olive oils in prestigious international
+        competitions.
+      </p>
+      <div class="list-maslina-1"></div>
+      <div class="list-maslina-2"></div>
+      <div class="list-maslina-3"></div>
+      <div class="list-maslina-4"></div>
+      <div class="list-maslina-5"></div>
+    </div>
+  </div>
+
+  <h1 class="products-h1">Our products</h1>
+
+  <div class="products">
+    <img src="../assets/batch_Romano-velika.png" alt="" />
+    <img src="../assets/batch_Istra-veliko.png" alt="" />
+    <img src="../assets/batch_Mlado-velika.png" alt="" />
+    <img src="../assets/batch_Organic-velika.png" alt="" />
+  </div>
+
+  <div class=""></div>
+
+  <div class="contact">
+    <GoogleMap />
+  </div>
+
+  <div class="footer">
+    <FooterVue />
+  </div>
 </template>
 
 <script>
+import NavBar from "@/components/NavBar.vue";
+import GoogleMap from "@/components/GoogleMaps.vue";
+import FooterVue from "@/components/FooterVue.vue";
+
 export default {
   name: "HomeView",
+  components: { NavBar, GoogleMap, FooterVue },
 };
 </script>
 
-<style></style>
+<style>
+@import url("../assets/style.css");
+</style>
