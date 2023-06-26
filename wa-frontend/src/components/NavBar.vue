41b0024ec59017d214013e786a83c33fe08db268
EricOstoni <eostoni@student.unipu.hr>
2023-06-26 03:25:09 +0200

    Navbar for HomeView

--- wa-frontend/src/components/NavBar.vue
+++ wa-frontend/src/components/NavBar.vue
@@ -1,17 +1,24 @@
 <template>
   <nav>
     <a class="logo">Oil Shop Ostoni</a>
-    <a class="first">About us</a>
-    <a class="second">Our products</a>
-    <a>Contact</a>
-
-    <button class="btn-navbar">WEBSHOP</button>
+    <a class="first" @click="scrollTo('about-us')">About us</a>
+    <a class="second" @click="scrollTo('products')">Our products</a>
+    <a @click="scrollTo('contact')">Contact</a>
+    <button class="btn-navbar" @click="toShop">WEBSHOP</button>
   </nav>
 </template>
 
 <script>
 export default {
   name: "NavBar",
+  methods: {
+    toShop() {
+      this.$router.push("/shop");
+    },
+    scrollTo(section) {
+      this.$emit('navigate', section);
+    },
+  },
 };
 </script>
 
