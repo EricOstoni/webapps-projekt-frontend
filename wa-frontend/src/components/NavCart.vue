2e06fa51f601bb13319f096f62f019ca32f3c90d
EricOstoni <eostoni@student.unipu.hr>
2023-06-26 03:25:52 +0200

    Navbar for shopping cart page

---
+++ wa-frontend/src/components/NavCart.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="navbar">
+    <div class="shop-name">
+      <a href="/shop">
+        <h1>Oil Shop</h1>
+      </a>
+    </div>
+
+    <p></p>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavCart",
+};
+</script>
+
+<style scoped>
+.navbar {
+  display: inline-flex;
+
+  padding: 10px;
+  background-color: rgb(103, 118, 23);
+}
+
+.shop-name h1 {
+  background-color: rgb(103, 118, 23);
+  font-size: 1.5em;
+}
+
+.shop-name a {
+  text-decoration: none;
+  color: black;
+}
+
+.icons {
+  display: flex;
+  flex-direction: row;
+  position: relative;
+  right: 0;
+  margin-left: auto;
+  width: 50px;
+  background-color: rgb(103, 118, 23);
+  gap: 15px;
+}
+
+.icons a {
+  color: #333;
+  text-decoration: none;
+}
+
+.icons a:hover {
+  color: white;
+}
+</style>
